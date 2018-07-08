aa91d40a320d120beac0263167178bd81c4eea79
Rico Crescenzio <rico.crescenzio@gmail.com>
2018-07-08 17:21:13 +0200

    fixed compilation error

--- WinkKit/Core/Presentation/Presenter/WKPresenter.swift
+++ WinkKit/Core/Presentation/Presenter/WKPresenter.swift
@@ -17,7 +17,7 @@ public protocol WKPresentableView: AnyObject {
 /// The base presenter protocol.
 public protocol WKPresenter: AnyObject {
     /// Every presenter can manage a view, which must conform to `WKPresentableView`
-    associatedtype View = PresentableView
+    associatedtype View = WKPresentableView
     
     /// The view managed by this presenter.
     ///
